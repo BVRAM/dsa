2c111f2bb5d3824b5a114f23e0c8028da6bd7833
BVRAM <sureshvenkat625@gmail.com>
2023-10-02 21:23:51 +0530

    Create sumOfNnumbers.cpp
    sum of n natural numbers

---
+++ Basics/sumOfNnumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int n, sum;
+	cout << "Enter a number to calculate sum: " << endl;
+	cin >> n;
+	sum = (n*(n+1))/2;
+	cout << "sum of natural numbers : " << sum << endl;
+
+}
