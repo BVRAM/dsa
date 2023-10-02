c8b34df7618b382d5376e529d1e2d4c11c485eb9
BVRAM <sureshvenkat625@gmail.com>
2023-10-02 21:59:18 +0530

    Create areaOfCircle.cpp
    Area of Circle

---
+++ Basics/areaOfCircle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+	int r, area;
+	cout << "Enter the radius: " << endl;
+	cin >> r;
+	area = (float)22/7*r*r;
+	cout << "Area of circle is : " << area << endl;
+}
