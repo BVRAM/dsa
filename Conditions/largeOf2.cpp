6b328cb0bb10cad444a5fd1e5c1b4bac13bea097
BVRAM <sureshvenkat625@gmail.com>
2023-10-03 11:51:54 +0530

    Create largeOf2.cpp
    largest of 2 numbers

---
+++ Conditions/largeOf2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+	cout << "Enter any 2 numbers :" << endl;
+	cin >> a >> b;
+	if(a>b){
+		cout << "a is bigger than b" << endl;
+	}else {
+		cout << "b is bigger than a" << endl;
+	}
+	return 0;
+}
